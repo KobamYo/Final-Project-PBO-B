9992a65bf80c740fd9a8bdde8e727952c27a46a2
Wahyu Tri Saputro <91375670+KobamYo@users.noreply.github.com>
2021-12-12 20:24:14 +0700

    Update GameFrame.java

--- Prototype Snake/src/GameFrame.java
+++ Prototype Snake/src/GameFrame.java
@@ -4,7 +4,7 @@ public class GameFrame extends JFrame
 {
 	GameFrame()
 	{
-		this.add(new GamePanel());
+		
 		this.setTitle("Snake");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
